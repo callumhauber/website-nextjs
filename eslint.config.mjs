cff2c80f7e1edc8ac31286799b1290c66a29aab3
Callum Hauber <callumhauber@pm.me>
2025-10-29 00:20:24 -0400

    configure a bunch of code formatting stuff

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,46 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  jsxA11y.flatConfigs.recommended,
+  // Plugins section
+  {
+    plugins: {
+      'jsx-a11y': jsxA11y,
+      'simple-import-sort': simpleImportSort,
+      'better-tailwindcss': eslintPluginBetterTailwindcss,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      ...eslintPluginBetterTailwindcss.configs['recommended-error'].rules,
+    },
+    settings: {
+      'better-tailwindcss': {
+        entryPoint: 'src/app/globals.css',
+      },
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
 ]);
 
